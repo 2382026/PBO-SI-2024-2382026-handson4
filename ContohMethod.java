39501a7125834ca1f99c7a8117941e4bd5918c9f
2382026 <2382026@unai.edu>
2024-09-24 10:10:45 +0700

    handson 4

---
+++ ContohMethod.java
@@ -0,0 +1,13 @@
+public class ContohMethod {
+
+    public static int hitungLuasPersegi(int sisi) {
+        int luas = sisi * sisi;
+        return luas;
+    }
+
+    public static void main(String[] args) {
+        int sisi = 5;
+        int luas = hitungLuasPersegi(sisi);
+        System.out.println("luas persegi dengan sisi" + sisi + " adalah " + luas);
+    }
+}
